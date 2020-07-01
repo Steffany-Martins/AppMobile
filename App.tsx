af84f304dc4932274d820c6e1e1469d8727d1326
Steffany <steffany@zee-dog.com>
2020-07-01 18:13:01 -0300

    svg toolbar button

--- App.tsx
+++ App.tsx
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/*Bar*/
+import { StatusBar } from "react-native";
+/**/
+/*React*/
+import React from "react";
+/** */
+/*App Loading*/
+import { AppLoading } from "expo";
+/***/
+/*Fontes */
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+/***/
+/*Routes*/
+import Routes from "./src/routes";
+/** */
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
