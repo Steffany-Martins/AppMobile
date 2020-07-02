ae139239f6704eba97f60169bee623f4fdbfa29f
Steffany <steffany@zee-dog.com>
2020-07-02 05:20:27 -0300

    start project

--- App.tsx
+++ App.tsx
@@ -1,1 +1,25 @@
+<<<<<<< HEAD
 import React from "react";
+=======
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> parent of af84f30... svg toolbar button
