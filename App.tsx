a21af1da00a0078464aa49501b486cc2153a340a
Steffany <steffany@zee-dog.com>
2020-07-02 05:24:29 -0300

    Revert "Revert "svg toolbar button""
    This reverts commit 707e2fd4698349d76316359a7afd7b8d8cb70a08.

--- App.tsx
+++ App.tsx
@@ -1,18 +1,51 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from "react";
 =======
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+=======
+/*Bar*/
+import { StatusBar } from "react-native";
+/**/
+/*React*/
+import React from "react";
+/** */
+/*App Loading*/
+import { AppLoading } from "expo";
+/***/
+/*Fontes */
+import { Ubuntu_700Bold, useFonts } from "@expo-google-fonts/ubuntu";
+import { Roboto_400Regular, Roboto_500Medium } from "@expo-google-fonts/roboto";
+/***/
+/*Routes*/
+import Routes from "./src/routes";
+/** */
+>>>>>>> parent of 707e2fd... Revert "svg toolbar button"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
   );
 }
+<<<<<<< HEAD
 
 const styles = StyleSheet.create({
   container: {
@@ -23,3 +56,5 @@ const styles = StyleSheet.create({
   },
 });
 >>>>>>> parent of af84f30... svg toolbar button
+=======
+>>>>>>> parent of 707e2fd... Revert "svg toolbar button"
